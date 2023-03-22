fdf6d58b1d90d747c358028d12423d2bbde8b1af
rosercode <rosercode@gmail.com>
2023-03-23 02:50:20 +0800

    refactor define Class-"Account", Encapsulates operations on the account table

--- app.py
+++ app.py
@@ -1,33 +1,37 @@
-from flask import Flask, request, Response, url_for, Blueprint
+from flask import Flask, request, Response, Blueprint
 from flask_cors import CORS
 from werkzeug.serving import run_simple
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 import json
 
-import mysql_db as db
+from mysql_db import AccountDAO
 
 bp = Blueprint('burritos', __name__,
-                        template_folder='templates')
+               template_folder='templates')
 
-@bp.route('/page',methods = ['POST', 'GET'])
+accountDao = AccountDAO()
+
+
+@bp.route('/page', methods=['POST', 'GET'])
 def page():  # put application's code here
     args = request.args
     pageNum = int(args.get('pageNum')) if args.get('pageNum') != None else 1
     pageSize = int(args.get('pageSize')) if args.get('pageSize') != None else 10
     q = args.get('q') if args.get('q') != None else ''
     responseData = {
-        "pageSize":pageSize,
-        "pageNum":pageNum,
-        "pageTotal": db.count(q),
-        "data": db.selectPage((pageNum-1)*pageSize, pageSize, q)
+        "pageSize": pageSize,
+        "pageNum": pageNum,
+        "pageTotal": accountDao.count(q),
+        "data": accountDao.selectPage((pageNum - 1) * pageSize, pageSize, q)
     }
     response = {
-        "code":200,
-        "data":responseData
+        "code": 200,
+        "data": responseData
     }
-    return Response(json.dumps(response),  mimetype='application/json')
+    return Response(json.dumps(response), mimetype='application/json')
 
-@bp.route('/add',methods = ['POST', 'GET'])
+
+@bp.route('/add', methods=['POST', 'GET'])
 def add():
     args = request.args
     obj = {
@@ -40,14 +44,15 @@ def add():
         'bind_phone': args.get('bind_phone'),
         'comment': args.get('comment')
     }
-    db.insert(obj)
+    accountDao.insert(obj)
     response = {
-        "code":200,
-        "data":""
+        "code": 200,
+        "data": ""
     }
-    return Response(json.dumps(response),  mimetype='application/json')
+    return Response(json.dumps(response), mimetype='application/json')
 
-@bp.route('/update',methods = ['POST', 'GET'])
+
+@bp.route('/update', methods=['POST', 'GET'])
 def update():
     args = request.args
     obj = {
@@ -61,22 +66,24 @@ def update():
         'bind_phone': args.get('bind_phone'),
         'comment': args.get('comment')
     }
-    db.update(obj)
+    accountDao.update(obj)
     response = {
-        "code":200,
-        "data":""
+        "code": 200,
+        "data": ""
     }
-    return Response(json.dumps(response),  mimetype='application/json')
+    return Response(json.dumps(response), mimetype='application/json')
 
-@bp.route('/delete',methods = ['POST', 'GET'])
+
+@bp.route('/delete', methods=['POST', 'GET'])
 def delete():  # put application's code here
     id = int(request.args.get('id'))
-    db.deleteById(id)
+    accountDao.deleteById(id)
     response = {
-        "code":200,
-        "data":""
+        "code": 200,
+        "data": ""
     }
-    return Response(json.dumps(response),  mimetype='application/json')
+    return Response(json.dumps(response), mimetype='application/json')
+
 
 app = Flask(__name__, static_url_path="/")
 # CORS configuation
